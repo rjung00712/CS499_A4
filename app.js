495d39d6847bd0af22332d4a262c2e8f5fb6a948
ryjung <richard.y.jung@gmail.com>
2017-03-12 18:27:42 -0700

    add interval polling and push to elastic search

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var elasticsearch = require('elasticsearch');
 var request = require('request');
 var parseString = require('xml2js').parseString;
 var universalStudiosWaitTimeURL = 'http://www.universalstudioshollywood.com/waittimes/?type=all&site=USH';
+var uuid = require('uuid/v4');
+
+const interval = 600000;
+const myIndex = 'wait-times';
 
 var client = elasticsearch.Client( {
    host: 'search-my-wait-time-es-vp44rjh7qhndk4uhw6wyaozr3a.us-west-1.es.amazonaws.com',
    log: 'info'
 });
 
+// checks connection status to elastic search
 client.ping({
     // ping usually has a 3000ms timeout
     requestTimeout: 5000
@@ -22,46 +27,60 @@ client.ping({
     }
 });
 
-// var getXmlWaitTime = function () {
-//     request(universalStudiosWaitTimeURL, function (error, response, body) {
-//         if (error) {
-//             console.error(error);
-//             console.log("getting XML Error");
-//         } else {
-//             // getJSON(body);
-//             var json = JSON.stringify(response);
-//             console.log('this worksed');
-//         }
-//     });
-// };
-
-
+// get all the ride wait times parse through json and put into array
 function fetchWaitingtimes() {
     request(universalStudiosWaitTimeURL, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            // console.log(body);
-
             var waitTimes = [];
 
             parseString(body, function (err, result) {
-                // console.dir(result.rss.channel[0].item);
                 var items = result.rss.channel[0].item;
                 for(var i = 0; i < items.length; i++) {
+                    var str = JSON.stringify((items[i].description[0]));
 
-                    waitTimes.push({
-                        'ride': items[i].title[0],
-                        'wait time': items[i].description[0]
-                    });
-                    console.log(waitTimes[i]);
-                    // console.log(items[i].title[0], items[i].description[0]);
-                    // putItem(items[i].title[0], items[i].description[0]);
+                    if(str.indexOf("min") > -1) {
+                        waitTimes.push({
+                            'ride': items[i].title[0],
+                            'wait_time': items[i].description[0],
+                            'time': Date.now()
+                        });
+                    }
                 }
             });
+            console.log(waitTimes);
+
+            sendToElasticSearch(waitTimes);
+
+        } else {
+            console.log(err);
         }
-    })
+    });
 }
 
+function sendToElasticSearch(waitTimes) {
+    for(var i = 0; i < waitTimes.length; i++) {
+        client.create({
+            index: myIndex,
+            type: 'Times',
+            id: uuid(),
+            body: waitTimes[i]
+        }, function(err, response) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(response);
+            }
+        });
+    }
+};
 
+// keep executing every ten minutes to collect wait time data
+function setTimeInterval() {
+    setInterval(function() {
+        fetchWaitingtimes();
+    }, interval)
+};
 
+setTimeInterval();
 
 
